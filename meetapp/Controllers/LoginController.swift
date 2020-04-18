3bbfbb31f8a8f8c89d3909b44183a6d085af233e
admin <admin@admins-Mac-mini.local>
2020-04-18 19:46:52 +0300

    added sqlite

--- meetapp/Controllers/LoginController.swift
+++ meetapp/Controllers/LoginController.swift
@@ -9,18 +9,60 @@
 import UIKit
 import FirebaseUI
 import FirebaseAuth
+import SQLite3
 
 class LoginController: UIViewController {
     
     @IBOutlet weak var teEmail: UITextField!
     @IBOutlet weak var tePassword: UITextField!
+    var userGroups: [Group] = []
     
     //@IBOutlet var tbEmail: UITextField!
     //@IBOutlet var tbPassword: UITextField!
     @IBOutlet weak var tbStatus: UILabel!
+    func getUserGroups(uid: String) {
+        DB.shared.groups.observeAll(onSuccess: { (groups:[Group]) in
+            for group in groups {
+                if group.owner == uid {
+                    self.userGroups.append(group)
+                }
+            }
+       })
+    }
         
     func saveUserAndGoHome() {
         Configuration.saveUserId(Auth.auth().currentUser!.uid)
+        var db: OpaquePointer?
+        var stmnt: OpaquePointer?
+        let fileURL = try!
+            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                .appendingPathComponent("myGroups.sqlite")
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            Helper.showToast(controller: self, message: "Error loading local data", seconds: 5)
+        }
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS Groups (id INTEGER PRIMARY KEY AUTOINCREMENT, guid TEXT)"
+        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK {
+            Helper.showToast(controller: self, message: "Error loading local data", seconds: 5)
+        }
+        
+        let insertQuery = "INSERT INTO Groups (guid) VALUES (?)"
+        if sqlite3_prepare(db, insertQuery, -1, &stmnt, nil) != SQLITE_OK {
+            Helper.showToast(controller: self, message: "Error loading local data", seconds: 5)
+        }
+        self.getUserGroups(uid: Auth.auth().currentUser!.uid)
+        for userGroup in userGroups {
+            if sqlite3_bind_text(stmnt, 1, userGroup.guid, -1, nil) != SQLITE_OK {
+                Helper.showToast(controller: self, message: "Error loading local data", seconds: 5)
+                return
+            }
+            if sqlite3_step(stmnt) != SQLITE_DONE {
+                Helper.showToast(controller: self, message: "Error saving local data", seconds: 5)
+                return
+            }
+            print("Added 1 item")
+        }
+        
         performSegue(withIdentifier: "goHome", sender: Any?.self)
     }
     
